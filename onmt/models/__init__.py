f30fbee62087e00d4bb21a7d929a0f5de329722b
atmyre <gaintseva.t@gmail.com>
2019-04-18 17:38:34 +0000

    fix bug

--- onmt/models/__init__.py
+++ onmt/models/__init__.py
@@ -1,6 +1,6 @@
 """Module defining models."""
-from onmt.models.model_saver import build_model_saver, ModelSaver
+from onmt.models.model_saver import build_model_saver, build_gan_saver, ModelSaver
 from onmt.models.model import NMTModel
 
-__all__ = ["build_model_saver", "ModelSaver",
+__all__ = ["build_model_saver", "build_gan_saver", "ModelSaver",
            "NMTModel", "check_sru_requirement"]
